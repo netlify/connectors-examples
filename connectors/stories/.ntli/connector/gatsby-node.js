3efd05ad29144ba4fbdfefc4daa36e09b5529732
Abhi Aiyer <abhiaiyer91@gmail.com>
2024-02-01 14:15:38 -0600

    initial

--- connectors/stories/.ntli/connector/gatsby-node.js
+++
@@ -1,7 +0,0 @@
-
-import { integration } from "./index.js";
-const { contentEngineImplementations } = integration.netlifyConnectPlugin;
-
-export const onPluginInit = contentEngineImplementations.onPluginInit;
-export const sourceNodes = contentEngineImplementations.sourceNodes;
-export const createSchemaCustomization = contentEngineImplementations.createSchemaCustomization;
--- connectors/stories/.ntli/site/netlify/functions/handler.js
+++
@@ -1,3 +0,0 @@
-import { integration } from '../integration';
-integration._slug = 'f9p163p2-jamhack-storis-connector';
-export const handler = integration.baseHandler;
